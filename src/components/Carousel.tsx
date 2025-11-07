ae042540b950c78bf83249eaa3717aac2f88a3cb
facuotero <facurimini@gmail.com>
2025-11-07 10:59:16 -0300

    feat(Carousel): integrate embla-carousel-auto-scroll for enhanced scrolling experience

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
-} from "@/components/ui/carousel"
-import Image from "next/image"
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import AutoScroll from "embla-carousel-auto-scroll";
 
 interface ImageItem {
   src: string;
@@ -16,24 +19,50 @@ interface CarouselSpacingProps {
   className?: string;
 }
 
-export function CarouselSpacing({ images, className = "w-full" }: CarouselSpacingProps) {
+export function CarouselSpacing({
+  images,
+  className = "w-full",
+}: CarouselSpacingProps) {
+  const trackItems = images.length ? [...images, ...images] : images;
+
   return (
-    <Carousel className={className}>
+    <Carousel
+      className={className}
+      opts={{
+        align: "start",
+        loop: true,
+        dragFree: true, 
+        duration: 25, 
+      }}
+      plugins={[
+        AutoScroll({
+          speed: 1.2,
+          startDelay: 0,
+          stopOnInteraction: false,
+          stopOnMouseEnter: true,
+        }),
+      ]}
+    >
       <CarouselContent className="-ml-1">
-        {images.map((image, index) => (
-          <CarouselItem key={index} className="pl-1 basis-1/2 md:basis-1/3 lg:basis-1/7">
+        {trackItems.map((image, index) => (
+          <CarouselItem
+            key={`${image.src}-${index}`}
+            className="pl-1 basis-1/2 md:basis-1/3 lg:basis-1/5 xl:basis-1/6"
+          >
             <div className="p-1 flex justify-center items-center">
               <Image
                 src={image.src}
                 alt={image.alt}
                 width={108}
                 height={68}
-                className={`opacity-70 hover:opacity-100 transition-opacity ${image.className || "h-17 w-108"}`}
+                className={`opacity-70 hover:opacity-100 transition-opacity ${
+                  image.className || "h-17 w-108"
+                }`}
               />
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
     </Carousel>
-  )
+  );
 }
